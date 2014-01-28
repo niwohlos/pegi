e908ec416e2cad049c0dd1b8bbb192ac5904e039
Alexander Kluth <kluth@freshcells.de>
2014-01-28 23:17:36 +0100

    Input file check
    Signed-off-by: Alexander Kluth <kluth@freshcells.de>

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        fprintf(stderr, "%s: no input files\n", argv[0]);
+        return -1;
+    }
+
     for (int i = 1; i < argc; i++)
     {
         FILE *fp = fopen(argv[i], "r");
