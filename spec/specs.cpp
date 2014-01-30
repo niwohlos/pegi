947f6bc9412f1bdb9249ff63fca873ed622287c3
Alexander Kluth <kluth@freshcells.de>
2014-01-30 23:08:30 +0100

    Added igloo
    Signed-off-by: Alexander Kluth <kluth@freshcells.de>

---
+++ spec/specs.cpp
@@ -0,0 +1,10 @@
+#include "igloo/igloo_alt.h"
+#include "tokenizerspec.h"
+
+
+using namespace igloo;
+
+int main(int argc, const char *argv[])
+{
+    return TestRunner::RunAllTests(argc, argv);
+}
---
+++ spec/tokenizerspec.h
@@ -0,0 +1,14 @@
+#ifndef _TOKENIZERSPEC_H
+#define _TOKENIZERSPEC_H
+
+#include "tokenize.hpp"
+#include "igloo/igloo_alt.h"
+
+using namespace igloo;
+
+Describe(tokenizer) {
+
+
+};
+
+#endif
